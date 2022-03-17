419221f8b1f153dae043064e5846f94908dbd230
foad pashah <neo_2000matrix@yahoo.com>
2022-03-17 17:29:07 +0330

    add constructor test for domains

---
+++ PasswordManagerTest/Domain/DTOs/PropertyGetResponseTest.cs
@@ -0,0 +1,22 @@
+﻿using PasswordManager.Domain.Domains;
+using PasswordManager.Domain.DTOs;
+using System;
+using Xunit;
+
+namespace PasswordManagerTest.Domain.DTOs
+{
+    public class PropertyGetResponseTest
+    {
+        [Fact]
+        public void Constructor_ShouldConstructPropertyProperly()
+        {
+            var property = new Property("TestName", "TestDesc", "TestValue", Guid.NewGuid());
+            var result = new PropertyGetResponse(property);
+            Assert.NotNull(result);
+            Assert.Equal(property.Name, result.Name);
+            Assert.Equal(property.Description, result.Description);
+            Assert.Equal(property.Id, result.Id);
+            Assert.Equal(property.Value,result.Value);
+        }
+    }
+}
